14913a1aff4f73a0c71673f1119ee2824f1eae18
Jonathan Danse <j.danse@prestaedit.com>
2026-05-15 20:25:23 +0200

    fix: use alpine:init event for persist plugin, remove standalone alpinejs

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,7 @@
 import './bootstrap';
 import 'preline';
-import Alpine from 'alpinejs'
 import Persist from '@alpinejs/persist'
-Alpine.plugin(Persist)
-window.Alpine = Alpine
-Alpine.start()
+
+document.addEventListener('alpine:init', () => {
+    window.Alpine.plugin(Persist)
+})
